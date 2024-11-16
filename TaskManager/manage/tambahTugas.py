6dae50e1bfcffc86c0e76c4de5e1b1ee425f75f0
deksaaa <annisafauziii3006@gmail.com>
2024-11-16 12:42:23 +0700

    Update tambahTugas.py

--- TaskManager/manage/tambahTugas.py
+++ TaskManager/manage/tambahTugas.py
@@ -1,12 +1,3 @@
 def tambahTugas(tasks):
-    while True:
-        judul = input("Masukkan judul tugas: ")
-        mataKuliah = input("Masukkan mata kuliah : ")
-        deadline = input("Masukkan deadline tugas (YYYY-MM-DD): ")
-        tasks.append({"Judul": judul, "Mata Kuliah": mataKuliah, "deadline": deadline, "status": "Belum selesai"})
-        print(f"Tugas '{judul}' berhasil ditambahkan!")
-        
-        ulangi = input("Apakah Anda ingin menambahkan tugas lagi? (y/n): ").lower()
-        if ulangi != "y" :
-            print("anda kembali ke menu utama")
-            break
+    tasks = TaskManager()
+    tasks.tambahTugas() 
