edcb4962f7e4dda85881b70678c2c91a73f2b055
sindikiamaharani <sindikiamaharani.303@gmail.com>
2024-11-16 12:51:35 +0700

    Update updateStatus.py

--- TaskManager/manage/updateStatus.py
+++ TaskManager/manage/updateStatus.py
@@ -1,13 +1,5 @@
+from models.operasi import TaskManager
+
 def update_status(tasks):
-    while True :
-        no_urut = int(input("Pilih nomor tugas yang ingin diperbarui statusnya: "))
-        if 1 <= no_urut <= len(tasks):
-            tasks[no_urut - 1]["status"] = "Selesai"
-            print(f"Tugas '{tasks[no_urut - 1]['Judul']}' telah selesai!")
-        else:
-            print("Nomor tugas tidak valid.")
-            
-        ulangi = input("Apakah Anda ingin mengubah status tugas lagi? (y/n): ").lower()
-        if ulangi != "y" :
-            print("anda kembali ke menu utama")
-            break
+    tasks = TaskManager()
+    tasks.update_status()
