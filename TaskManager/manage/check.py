ae91222a09f262c98837a5f7db3eb6b244ff6c5c
Baysarah <158597300+sarrvibes@users.noreply.github.com>
2024-11-16 12:47:16 +0700

    Update check.py

--- TaskManager/manage/check.py
+++ TaskManager/manage/check.py
@@ -1,17 +1,6 @@
 from datetime import datetime, timedelta
+from models.operasi import TaskManager
 
-# Fungsi untuk menampilkan tugas yang mendekati deadline
 def check_deadline(tasks, days_before=2):
-    today = datetime.today().date()
-    pengingat = today + timedelta(days=days_before)
-    
-    for tugas in tasks:
-        deadlineTugas = datetime.strptime(tugas["deadline"], "%Y-%m-%d").date()
-        
-        if today > deadlineTugas:
-            print(f"⚠️ Tugas '{tugas['Judul']}' sudah lewat deadline ❗")
-        elif today <= deadlineTugas <= pengingat:
-            print(f"🔔 Tugas '{tugas['Judul']}' mendekati deadline pada {tugas['deadline']}❗❗")
-        else:
-            print(f"👌 Tugas '{tugas['Judul']}' belum mendekati deadline.")
-
+    tasks = TaskManager()
+    tasks.check_deadline(days_before)
