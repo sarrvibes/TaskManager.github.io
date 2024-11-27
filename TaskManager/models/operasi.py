654acc0a504eaeb5b3d2473625a913b2754493a0
jackboyss <151706058+jackboyss@users.noreply.github.com>
2024-11-28 00:15:42 +0700

    Update operasi.py

--- TaskManager/models/operasi.py
+++ TaskManager/models/operasi.py
@@ -36,7 +36,6 @@ class TaskManager:
             print("=" * 25 + " DAFTAR TUGAS " + "=" * 25)
             for i, task in enumerate(self.tasks, start=1):
                 print(f"{i}. {task}")
-                
                 def hapus_tugas(self):
         while True:
             if not self.tasks:
